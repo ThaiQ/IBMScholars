73836fd4f870615e59c830401dd55d93f907f8d5
ThaiQ <quachhongthai200@gmail.com>
2020-07-18 16:24:12 -0700

    deployment scripts

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          THIS IS THAI, DONT TOUCH!
         </p>
         <a
           className="App-link"
