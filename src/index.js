8eb989db3b92743e4f0b5ca628a87f03c40b060b
Seema Vora <seema.s.vora@gmail.com>
2020-07-23 15:42:09 -0700

    Adds A Homepage Image

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Routing from './routes'
 import './index.css'
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 export default function App(props) {
 
