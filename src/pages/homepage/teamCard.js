dcbe3f9d6d340113b67bbd300ba2d3470e43c403
Seema Vora <seema.s.vora@gmail.com>
2020-07-27 10:48:12 -0700

    Removes Extra Spaces

--- src/pages/homepage/teamCard.js
+++ src/pages/homepage/teamCard.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import {CardImg, Card, CardBody, CardTitle, CardText} from 'reactstrap';
+import { CardImg, Card, CardBody, CardTitle, CardText } from 'reactstrap';
 
 import './teamCard.css';
-
 
 class TeamCard extends React.Component {
   render() {
     return (
-   
-        <div className = 'card-wrapper'>
-      <Card >
-        <CardImg className = 'pic-dim'src={this.props.creators.pic} alt="Card image cap" />
-        <CardBody>
-          <CardTitle>{this.props.creators.name}</CardTitle>
-          <CardText font-size = '50%'>B.E. Software Engineering at SJSU</CardText>
-          <a href={this.props.creators.Linkedin}><i class="fa fa-linkedin"></i></a>
-        </CardBody>
-      </Card>
-    </div>
 
-
-     
+      <div className='card-wrapper'>
+        <Card >
+          <CardImg className='pic-dim' src={this.props.creators.pic} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>{this.props.creators.name}</CardTitle>
+            <CardText font-size='50%'>B.E. Software Engineering at SJSU</CardText>
+            <a href={this.props.creators.Linkedin}><i class="fa fa-linkedin"></i></a>
+          </CardBody>
+        </Card>
+      </div>
     );
   }
 }
