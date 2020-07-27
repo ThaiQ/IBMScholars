903f41e33aed9026dbdacc9075088cf0164c6453
ThaiQ <quachhongthai200@gmail.com>
2020-07-27 11:32:57 -0700

    Revert "Creates Homepage (#7)" (#8)
    This reverts commit 1c4e096334dab5261667ee0e24cb29e2aa518528.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import ReactDOM from 'react-dom';
 import Routing from './routes'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 export default function App(props) {
 
--- src/pages/homepage/home.js
+++ src/pages/homepage/home.js
@@ -1,92 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './home.css';
-import titlePage from './pictures/titlePage.png'
-import TeamCard from './teamCard'
+
+import SampleMerchant from '../sample_merchant-page/test'
 import UserNavbar from '../../components/navbar/navbar';
-import nanar from '../homepage/pictures/nanar.jpeg';
-import annabel from '../homepage/pictures/annabel.jpeg';
-import seema from '../homepage/pictures/seema.jpeg'
-import surabhi from '../homepage/pictures/surabhi.png'
-import thai from '../homepage/pictures/thai.png'
 
-
-export default class Home extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeTab: '1',
-
-      creators: [
-        {
-          name: 'Nanar Boursalian',
-          Linkedin: 'https://www.linkedin.com/in/nanarboursalian/',
-          pic: nanar
-        },
-        {
-          name: 'Seema Vora',
-          Linkedin: 'https://www.linkedin.com/in/seemasvora/',
-          pic: seema
-        },
-        {
-          name: 'Thai Quach',
-          Linkedin: 'https://www.linkedin.com/in/thai-quach/',
-          pic: thai
-        },
-        {
-          name: 'Surabhi Gupta',
-          Linkedin: 'https://www.linkedin.com/in/gupta-surabhi/',
-          pic: surabhi
-        },
-        {
-          name: 'Annabel Kusumo',
-          Linkedin: 'https://www.linkedin.com/in/annabelkusumo/',
-          pic: annabel
-        }
-      ]
-    };
-  }
-
-  render() {
-    let creatorCards = this.state.creators.map(creators => {
-      return (
-        <li className="flex-item">
-          <TeamCard creators={creators} />
-        </li>
-
-      )
-    })
-    return (
-      <div className='overview' style={{ backgroundColor: 'blue' }}>
-        <UserNavbar />
-        <div style={{ width: '100%', margin: 'auto' }}>
-          <div className="landing-grid">
-            <img
-              src={titlePage}
-              className="homepage-img"
-              alt="homepage image"
-            />
-            <div className="banner-text">
-              <h1>About Us</h1>
-              <hr />
-              <p className='lead text-center'>
-                {' '}
-                Every child deserves the same opportunities. Our program hopes to give low-income elementary school kids with disabilities
-                a personalized service, which teaches them primary school topics geared to fit their needs. Our platform is about creating a community
-                for these students.
-            </p>
-            </div>
-            <div className='meet-header'>
-              Meet the Team
-            </div>
-            <p className='info'>Our team includes 5 young developers who hope to make a positive difference
-              using their technical skills. This platform runs entirely on the IBM cloud.
-            </p>
-            <ul className="flex-container wrap-reverse">
-              {creatorCards}
-            </ul>
-          </div>
-        </div>
-      </div>
-    )
-  }
+export default function Home() {
+  return (
+    <div className="App">
+      <UserNavbar />
+      <header className="App-header">
+        <p>
+          THIS IS THAI, YOU CAN TOUCH NOW!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <SampleMerchant />
+      </header>
+    </div>
+  );
 }
