a037f6f70914fcee1c3f3d6babfce211e89f2689
Seema Vora <seema.s.vora@gmail.com>
2020-07-28 00:15:31 -0700

    Adds ADHD and Autism Page

---
+++ src/pages/Disabilities/ADHD/adhd.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Navbar from "../../../components/navbar/navbar.js";
+import GameLayout from '../Autism/gameLayout.js'
+import './adhd.css'
+
+
+
+
+
+export default class ADHD extends Component {
+
+  render() {
+    return (
+      <div className='adhd-page'>
+        <Navbar />
+        <div style={{ width: '100%', margin: 'auto' }}>
+          <div className='adhd-page'>
+
+           
+        <div className="banner-text">
+              <h1>About Us</h1>
+              <hr />
+              <p className='lead text-center'>
+                {' '}
+                THIS PAGE IS CURRENTLY UNDER CONSTRUCTION
+            </p>
+            
+            </div>
+            </div>
+        
+          </div>
+        </div>
+        )
+      }
+    }
