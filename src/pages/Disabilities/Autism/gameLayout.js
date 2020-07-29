4138ce39061451457cc1554324b522f0e6774ae5
ThaiQ <quachhongthai200@gmail.com>
2020-07-28 19:31:41 -0700

    Refactored Link for cloud depployment

--- src/pages/Disabilities/Autism/gameLayout.js
+++ src/pages/Disabilities/Autism/gameLayout.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { CardImg, Card, CardBody, CardTitle, Button } from 'reactstrap';
-
+import { Link } from "react-router-dom";
 import './gameLayout.css';
 
 export default class GameLayout extends Component {
   render() {
     return (
       <div className='card-wrapper'>
-        <Card >
+        <Card style={{cursor:'default'}}>
           <CardImg className='pic-dim' src={this.props.activities.pic} alt="Card image cap" />
           <CardBody>
             <CardTitle className='text-set'>{this.props.activities.name}</CardTitle>
-            <Button href={this.props.activities.link}><i>Clear Here to Begin!</i></Button>
+            <Button><Link to={this.props.activities.link}>Click Here to Begin!</Link></Button>
           </CardBody>
         </Card>
       </div>
